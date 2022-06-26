a579ce6ad177ae06b818ff89d5b5a54ef306b393
OmarYasserMaher <omar.yasser.maher@gmail.com>
2022-06-26 18:54:38 +0200

    fix issue

--- mysite/runserver.bat
+++ mysite/runserver.bat
@@ -1,7 +1,6 @@
-python manage.py migrate && ^
-python manage.py makemigrations && ^
-python manage.py migrate && ^
-python manage.py loaddata countries.json && ^
-python manage.py loaddata states.json && ^
-python manage.py loaddata areas.json && ^
+python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata countries.json
+python manage.py loaddata states.json
+python manage.py loaddata areas.json
 python manage.py runserver
